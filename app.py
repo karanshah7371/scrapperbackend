ff99be9c9a1f5b47f06157bc1ff0f51d2048d27c
Karan Shah <karanshah737@gmail.com>
2022-02-01 13:19:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -123,7 +123,12 @@ def urllist():
     listurl= mongoConnect("getallurl")
     return jsonify(listurl)
 
-
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  return response
 
 if __name__=="__main__":
     app.run()
